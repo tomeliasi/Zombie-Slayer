acd0fc73a9c56b582937201af1011f97fc50fafb
dor shaul <dor.asti@gmail.com>
2023-08-25 15:05:37 +0300

    fix the resources uploded bug

--- Zombie Slayer (2019)/Properties/Resources.Designer.cs
+++ Zombie Slayer (2019)/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Zombie_Slayer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Zombie_Slayer__2019_.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Zombie_Slayer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
